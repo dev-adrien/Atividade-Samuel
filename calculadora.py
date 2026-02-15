1c0c59b2f19077c167e9d2140f38734ef802468c
adrien <adrienbzr@outlook.com>
2026-02-14 23:35:19 -0300

    Adicionando verificação de variável ambiente

--- calculadora.py
+++ calculadora.py
@@ -1,3 +1,17 @@
 n1 = int(input("Digite um número: "))
 n2 = int(input("Digite outro número: "))
 op = input("Digite a operação (+, -, *, /): ")
+
+if op == "+":
+    print(f"{n1} + {n2} = {n1 + n2}")
+elif op == "-":
+    print(f"{n1} - {n2} = {n1 - n2}")
+elif op == "*":
+    print(f"{n1} * {n2} = {n1 * n2}")
+elif op == "/":        
+    if n2 != 0:
+        print(f"{n1} / {n2} = {n1 / n2}")     
+    else:       
+        print("Erro: Divisão por zero não é permitida.")
+else:
+    print("Operação inválida. Por favor, escolha entre +, -, *, /.")
